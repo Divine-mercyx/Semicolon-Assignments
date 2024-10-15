2bea7dbff95f5a8e1a6f3ac8c267bdce1da2ef5a
Divine Obinali <divinewilson766@gmail.com>
2024-10-15 14:34:07 +0100

    added Million.java to java folder

---
+++ java/Million.java
@@ -0,0 +1,7 @@
+public class Million {
+	public static void main(String[] args) {
+		System.out.println("i'll use half of it to start a business, the rest?! jaiye");
+		System.out.print("i'll use half of it to start a business, the rest?! jaiye\n");
+		System.out.printf("%s", "i'll use half of it to start a business, the rest?! jaiye mi");
+	}
+}
