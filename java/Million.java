6c8edf0870391befc92487c9d57d76a5014638e6
Divine Obinali <divinewilson766@gmail.com>
2024-10-15 15:01:38 +0100

    deleted Million.java

--- java/Million.java
+++
@@ -1,7 +0,0 @@
-public class Million {
-	public static void main(String[] args) {
-		System.out.println("i'll use half of it to start a business, the rest?! jaiye");
-		System.out.print("i'll use half of it to start a business, the rest?! jaiye\n");
-		System.out.printf("%s", "i'll use half of it to start a business, the rest?! jaiye mi");
-	}
-}
